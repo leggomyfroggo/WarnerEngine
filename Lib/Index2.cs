bb376ce12cb21e2800d2bd892a52c80671f76349
Chris Hackmann <cphackm@gmail.com>
2021-02-04 21:17:39 -0800

    Made Index2 serializable

--- Lib/Index2.cs
+++ Lib/Index2.cs
@@ -4,8 +4,8 @@
     {
         public static readonly Index2 Zero = new Index2(0, 0);
 
-        public int X { get; private set; }
-        public int Y { get; private set; }
+        public int X { get; set; }
+        public int Y { get; set; }
 
         public Index2(int X, int Y)
         {
