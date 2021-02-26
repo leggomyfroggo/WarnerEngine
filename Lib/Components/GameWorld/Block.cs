90e202279ddf1c54c96ecefe689f9557fe6b1e81
Chris Hackmann <cphackm@gmail.com>
2021-02-25 23:43:40 -0800

    [World] Added new Block and BlockFace classes as future replacements for WorldTile and friends

---
+++ Lib/Components/GameWorld/Block.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Xna.Framework;
+
+using WarnerEngine.Lib.Entities;
+using WarnerEngine.Lib.Helpers;
+using WarnerEngine.Services;
+
+namespace WarnerEngine.Lib.Components.GameWorld
+{
+    public class Block : IWorldSerializable, ISceneEntity, IDraw
+    {
+        private BackingBox backingBox;
+
+        public int TileSize;
+
+        public int X;
+        public int Y;
+        public int Z;
+
+        public int Width;
+        public int Height;
+        public int Depth;
+
+        public BlockFace[] Tiles;
+
+        public Block(int X, int Y, int Z, int Width, int Height, int Depth, BlockFace[] Tiles)
+        {
+            TileSize = 16;
+            this.X = X;
+            this.Y = Y;
+            this.Z = Z;
+            this.Width = Width;
+            this.Height = Height;
+            this.Depth = Depth;
+            this.Tiles = Tiles;
+            PostSerialize();
+        }
+
+        public void OnAdd(Scene ParentScene) { }
+
+        public void OnRemove(Scene ParentScene) { }
+
+        public void Draw()
+        {
+            IRenderService rs = GameService.GetService<IRenderService>();
+
+            Vector2 projectedPosition = backingBox.GetFullProjectedPosition2();
+            Rectangle bounds = new Rectangle(
+                (int)projectedPosition.X, 
+                (int)projectedPosition.Y, 
+                Width * TileSize, 
+                (Height + Depth) * TileSize
+            );
+            Rectangle plotter = new Rectangle(bounds.Left, bounds.Top, TileSize, TileSize);
+            foreach (BlockFace face in Tiles)
+            {
+                if (!face.Equals(BlockFace.Empty))
+                {
+                    rs.DrawQuad(
+                        face.TextureKey,
+                        plotter,
+                        new Rectangle(face.SourceX, face.SourceY, TileSize, TileSize)
+                    );
+                }
+                plotter.X += TileSize;
+                if (plotter.X >= bounds.Right)
+                {
+                    plotter.X = bounds.Left;
+                    plotter.Y += TileSize;
+                }
+            }
+        }
+
+        public BackingBox GetBackingBox()
+        {
+            return backingBox;
+        }
+
+        public bool IsVisible()
+        {
+            return true;
+        }
+
+        public void PostSerialize()
+        {
+            backingBox = new BackingBox(
+                BackingBox.IType.Static,
+                X,
+                Y,
+                Z,
+                Width * TileSize,
+                Height * TileSize,
+                Depth * TileSize
+            );
+        }
+    }
+}
---
+++ Lib/Components/GameWorld/BlockFace.cs
@@ -0,0 +1,20 @@
+﻿namespace WarnerEngine.Lib.Components.GameWorld
+{
+    public struct BlockFace
+    {
+        public static readonly BlockFace Empty = new BlockFace();
+
+        public string TextureKey;
+        public int SourceX;
+        public int SourceY;
+
+        public BlockFace(string TextureKey, int SourceX, int SourceY)
+        {
+            this.TextureKey = TextureKey;
+            this.SourceX = SourceX;
+            this.SourceY = SourceY;
+        }
+
+        // TODO: Add support for additional layers(water, decals, etc)
+    }
+}
