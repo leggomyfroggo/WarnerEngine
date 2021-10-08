448d75f197ee8f9c0a616a46667be4c8777ee48a
Chris Hackmann <cphackm@gmail.com>
2021-10-07 23:07:15 -0700

    Create render targets in NewSceneService

--- Services/Implementations/NewSceneService.cs
+++ Services/Implementations/NewSceneService.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using Microsoft.Xna.Framework.Graphics;
+
 using WarnerEngine.Lib.SceneGraph;
 
 namespace WarnerEngine.Services.Implementations
 {
     public class NewSceneService : INewSceneService
     {
+        public static class RenderTargets
+        {
+            public const string DepthPrimary = "scene_depth";
+            public const string CompositeSecondary = "scene_composite_secondary";
+            public const string CompositeTertiary = "scene_composite_tertiary";
+        }
+
         public IScene CurrentScene { get; private set; }
 
         private Dictionary<string, IScene> _scenes;
@@ -23,6 +32,33 @@ namespace WarnerEngine.Services.Implementations
 
         public void Initialize() 
         {
+            GameService.GetService<IEventService>().Subscribe(
+                Events.INTERNAL_RESOLUTION_CHANGED,
+                _ =>
+                {
+                    IRenderService renderService = GameService.GetService<IRenderService>();
+                    renderService
+                        .AddRenderTarget(
+                            RenderTargets.DepthPrimary,
+                            renderService.InternalResolutionX,
+                            renderService.InternalResolutionY,
+                            RenderTargetUsage.PreserveContents
+                        )
+                        .AddRenderTarget(
+                            RenderTargets.CompositeSecondary,
+                            renderService.InternalResolutionX,
+                            renderService.InternalResolutionY,
+                            RenderTargetUsage.PreserveContents
+                        )
+                        .AddRenderTarget(
+                            RenderTargets.CompositeTertiary,
+                            renderService.InternalResolutionX,
+                            renderService.InternalResolutionY,
+                            RenderTargetUsage.PreserveContents
+                        );
+                }
+            );
+
             _scenes = new Dictionary<string, IScene>();
         }
 
