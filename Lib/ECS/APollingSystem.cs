806290674de4203a1ef5f15bfcdf1b827e177d25
Chris Hackmann <cphackm@gmail.com>
2021-10-12 21:03:30 -0700

    Renamed the basic system implementation to reflect its polling nature

---
+++ Lib/ECS/APollingSystem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+using WarnerEngine.Services;
+
+namespace WarnerEngine.Lib.ECS
+{
+    public abstract class APollingSystem<TC1> : ISystem where TC1 : IComponent
+    {
+        public void Process()
+        {
+            ProcessImplementation(GameService.GetService<IECSService>().GetEntitiesWithComponent<TC1>());
+        }
+
+        public abstract void ProcessImplementation(IEnumerable<UInt64> Entities1);
+    }
+
+    public abstract class APollingSystem<TC1, TC2> : ISystem where TC1 : IComponent where TC2 : IComponent
+    {
+        public void Process()
+        {
+            ProcessImplementation(
+                GameService.GetService<IECSService>().GetEntitiesWithComponent<TC1>(),
+                GameService.GetService<IECSService>().GetEntitiesWithComponent<TC2>()
+            );
+        }
+
+        public abstract void ProcessImplementation(IEnumerable<UInt64> EntityIDs1, IEnumerable<UInt64> EntityIDs2);
+    }
+}
